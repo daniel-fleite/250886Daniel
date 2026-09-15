2b999146c51f70cf0bb602e13edb786af6d903a0
daniel-fleite <daniferazleite@gmail.com>
2026-09-15 20:19:20 -0300

    second commit

--- src/app/produto-detalhe/produto-detalhe.ts
+++ src/app/produto-detalhe/produto-detalhe.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+interface Products {
+  id: number;
+  nome: string;
+  preco: number;
+  descricao: string;
+  emEstoque: boolean;
+}
+
 @Component({
   selector: 'app-produto-detalhe',
   standalone: false,
@@ -8,11 +16,37 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './produto-detalhe.html',
 })
 export class ProdutoDetalhe {
-    id: string | null = null;
+
+  produto: Products | undefined;
+
+  produtos: Products[] = [
+    {
+      id: 1,
+      nome: `Camiseta`,
+      preco: 79.99,
+      descricao: `Camiseta lisa`,
+      emEstoque: true
+    },
+    {
+      id: 2,
+      nome: `Bermuda`,
+      preco: 59.99,
+      descricao: `Bermuda longa`,
+      emEstoque: true
+    },
+    {
+      id: 3,
+      nome: `Moletom`,
+      preco: 109.99,
+      descricao: `Moletom canguru`,
+      emEstoque: false
+    }
+  ];
 
   constructor(private route: ActivatedRoute) {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.id = this.route.snapshot.paramMap.get('id');
+    this.produto = this.produtos.find(produto => produto.id === id);
 
   }
 }
